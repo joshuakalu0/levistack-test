999c81fba580f4d24230b7a101ddb42f0532c422
Aju Joshua kalu <ajujoshua2019@gmail.com>
2025-04-30 02:45:49 +0100

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 "use client";
-import Navbar from "@/components/layout/navbar";
-import Hero from "@/components/sections/hero";
-import Features from "@/components/sections/features";
-import Pricing from "@/components/sections/pricing";
-import Testimonials from "@/components/sections/testimonials";
-import Footer from "@/components/layout/footer";
+
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Pricing />
-      <Testimonials />
-      <Footer />
+     home
     </main>
   );
 }
